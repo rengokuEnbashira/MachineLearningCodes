3dcc4a22cc35e6932aa8e621e47175fb334a4155
elvinMark <elvin.mark.titech@gmail.com>
2020-11-06 11:30:21 +0900

    Updating NeuralNetworks.py

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -14,29 +14,28 @@ class MultilayerPerceptron(nn.Module):
         self.relu = nn.ReLU()                            # Input Layer <-> Hidden Layer (Activation Function - give non linearity to the previous transformation)
         self.fc2 = nn.Linear(hidden_size,output_size)    # Hidden Layer <-> Output Layer (Linear Transformation)
     def forward(self,x):
-        o = self.fc1(x)  # eval first linear transformation to the input
-        o = self.relu(o) # apply activation function to the previous result
-        o = self.fc2(o)  # eval second linear transformation to the previous result
+        o = self.fc1(x)                                  # eval first linear transformation to the input
+        o = self.relu(o)                                 # apply activation function to the previous result
+        o = self.fc2(o)                                  # eval second linear transformation to the previous result
         return o
 
 class ConvNN(nn.Module):
     def __init__(self,):
-        super(ConvNN,self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1,10,kernel_size=3,padding=2),
-            nn.BatchNorm2d(10),
-            nn.ReLU(),
-            nn.MaxPool2d((2))
+        super(ConvNN,self).__init__()                # Calling super class (nn.Module) constructor
+        self.layer1 = nn.Sequential(                 # Create a sequential model for first layer
+            nn.Conv2d(1,10,kernel_size=3,padding=0), # Convolutional layer: 1 x 8 x 8 (x) 10 x 3 x 3 -> 10 x 6 x 6
+            nn.BatchNorm2d(10),                      # Batch normalization (s relu does not blow up)
+            nn.ReLU(),                               # Activation Function: ReLU
+            nn.MaxPool2d((2))                        # Max Pooling: 10 x 6 x 6 -> 10 x 3 x 3
         )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(10,20,kernel_size=3,padding=2),
-            nn.BatchNorm2d(20),
-            nn.ReLU(),
+        self.layer2 = nn.Sequential(                 # Second Layer
+            nn.Conv2d(10,20,kernel_size=3,padding=0),# Convolution Layer: 10 x 3 x 3 (x) 2 x 3 x 3 -> 20 x 1 x 1
+            nn.ReLU(),                               # Activation Function: ReLU
         )
-        self.fc = nn.Linear(20,10)
+        self.fc = nn.Linear(20,10)                   # Fully connected layer
     def forward(self,x):
         o = self.layer1(x)
-        o = self.layer2(x)
+        o = self.layer2(o)
         o = o.view(o.size(0),-1)
         o = self.fc(o)
         return o
@@ -64,7 +63,7 @@ if sys.argv[1] == "mlp":
     input_size = 64
     hidden_size = 20
     num_classes = 10
-    num_epochs = 20
+    num_epochs = 30
     batch_size = 100
     learning_rate = 0.01
 
@@ -97,10 +96,11 @@ if sys.argv[1] == "mlp":
     print(y_test_tensor[:30])
 
 elif sys.argv[1] == "cnn":
-    num_epochs = 20
+    num_epochs = 10
     batch_size = 100
     learning_rate = 0.01
-    
+
+    x = x.reshape(1797,1,8,8)
     x_train, x_test, y_train, y_test = train_test_split(x,y)
 
     x_train_tensor = torch.tensor(x_train).float() # converting x train data to tensor
